d83b1b126c3999d7155ae8c296e0d77371df044f
sile2510 <sile2510>
2019-02-27 17:53:18 +0100

    Novo nakon fetcha

---
+++ ConsoleApp1/5.3.8__7-3_koliko/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._3._8__7_3_koliko
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int koliko = 0;
+        }
+    }
+}
