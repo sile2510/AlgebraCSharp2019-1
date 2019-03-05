600e1ca4afb54f30caf6d0ebed34e68768df4840
sile2510 <sile2510>
2019-03-05 18:48:48 +0100

    jos zadataka

---
+++ ConsoleApp1/7.2.2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7._2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pozz("Boris");
+            Pozz("Boris", "Šipoš");
+            Console.ReadKey();
+        }
+        static void Pozz(string ime)
+        {
+            Console.WriteLine("Bok, " + ime + "!");
+        }
+        static void Pozz(string ime, string prezime)
+        {
+            Console.WriteLine("Bok, " +ime+" " +prezime+ "!");
+        }
+    }
+}
---
+++ ConsoleApp1/7.2_potprogrami/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7._2_potprogrami
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorld();
+            Program.HelloWorld();
+
+            Console.WriteLine(HelloWorld("bok"));
+            Console.ReadKey();
+        }
+        static void HelloWorld()
+        {
+            Console.WriteLine("Helloooooooooooooooooooooo");
+        }
+        static String HelloWorld(String poz)
+        {
+            return poz + " i tebi ";
+        }
+    }
+}
