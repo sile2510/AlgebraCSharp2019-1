5d7f9ef1cc57385c807fea19003de3d879ad6e95
sile2510 <sile2510>
2019-03-12 17:35:05 +0100

    out

---
+++ ConsoleApp1/8.2.2.2_klucna_rijec_out/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8._2._2._2_klucna_rijec_out
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Inicijaliziraj(out a);
+            Console.WriteLine("a = {0}", a);
+            Console.ReadKey();
+        }
+
+        private static void Inicijaliziraj(out int a)
+        {
+            a = 1;
+        }
+    }
+}
