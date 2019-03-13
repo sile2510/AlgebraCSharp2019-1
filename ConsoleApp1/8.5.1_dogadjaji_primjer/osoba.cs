64fd261be28f611884e69e677ac7b99ef412ec84
sile2510 <sile2510>
2019-03-13 17:19:59 +0100

    delegat_bog_ti_pomogao

---
+++ ConsoleApp1/8.5.1_dogadjaji_primjer/osoba.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8._5._1_dogadjaji_primjer
+{
+    class Osoba
+    {
+        public delegate void NaPromjenuImenaDelegat(object sender, EventArgs e);
+        public event NaPromjenuImenaDelegat NaPromjenuImena;
+
+        private string ime;
+        public string Ime
+        {
+            get { return ime; }
+            set
+            {
+                ime = value;
+                if (NaPromjenuImena != null)
+                {
+                    NaPromjenuImena(this, new EventArgs());
+                }
+            }
+
+        }
+
+
+    }
+}
